dfd3bccb75c9bc5768db80f736cb0867abe0807a
unknown <rahulsurana37@gmail.com>
2020-06-12 07:44:24 +0530

    Added salary count

--- emp.sh
+++ emp.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
-a=2
-b=3
-echso $a
-echo "Hellow world"
+isPresent=0;
+wageperHr=20;
+totalHr=8;
+randomcheck=$((RANDOM%2))
+if [[ $randomcheck -eq $ispresent ]];
+then
+	echo "Employee is present"
+	Salary=$(($totalHr * $wageperHr))
+	echo $Salary
+else
+	echo "Employee is absent"
+fi
