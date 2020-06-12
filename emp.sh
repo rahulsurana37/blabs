ed982254ab0e1440add5643c2ed59bba7c6921e6
unknown <rahulsurana37@gmail.com>
2020-06-12 07:49:13 +0530

    emp.sh

--- emp.sh
+++ emp.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
 
-a=2
-echo $a
-echo "Hellow world"
+isPresent=0;
+wageperHr=20;
+parttime=8;
+randomcheck=$((RANDOM%2))
+if [[ $randomcheck -eq $ispresent ]];
+then
+	echo "Employee is parttime"
+	Salary=$(($parttime * $wageperHr))
+	echo $Salary
+else
+	echo "Employee is absent"
+fi
