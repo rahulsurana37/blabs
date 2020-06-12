b663c1c2f1c9be664cd32aae1a3f81baa918db58
unknown <rahulsurana37@gmail.com>
2020-06-12 07:55:22 +0530

    Monthly Wage calculator

--- emp.sh
+++ emp.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
-a=2
-echo $a
-echo "Hellow world"
+isPresent=1;
+isAbsent=2;
+wagePerHr=20;
+perDayHr=8;
+Max_working_days=20;
+Max_working_Hrs=100;
+
+for (( days = 0; days <= $Max_working_days; days++ )); do
+checkattendence=$((RANDOM%3))
+                
+        case $checkattendence in
+                 1)
+                                echo "Employee is Working as a full time"
+                                WorkingHrs=8;
+                                Salary=$(($wagePerHr * $WorkingHrs))
+                                echo -e "Full time Employee Salary is: $Salary"
+                                ;;
+                                
+                2)
+                                echo "Employee is working as part time"
+                                WorkingHrs=4;
+                                Salary=$(($wagePerHr * $WorkingHrs))
+                                echo -e "Part time Employee Salary is: $Salary"
+                                ;;
+                0)
+                                echo "Employee is absent"
+                                WorkingHrs=0;
+                                Salary=0;
+                                ;;
+
+        esac
+
+
+        #statements
+done
