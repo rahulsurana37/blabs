4810ff201c8860e7fe0968cb28dab332218a163f
unknown <rahulsurana37@gmail.com>
2020-06-12 07:52:28 +0530

    Switch Case statement

--- emp.sh
+++ emp.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
-a=2
-echo $a
-echo "Hellow world"
+isPartTime=1;
+isFullTime=2;
+employeeRateHr=20;
+
+checkValue=$((RANDOM%3))
+
+case $checkValue in
+	$isPartTime)
+	 hrs=4;;
+	$isFullTime)
+	 hrs=8;;
+	*)
+	hrs=0;;
+esac
+Salary=$(($hrs * $employeeRateHr))
